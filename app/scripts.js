39c55b47e28bfa3e576c303d9a7972f5d30f1ae4
Wanhenri <slipkorn.jk@gmail.com>
2020-08-30 17:56:55 -0300

    parte 4

--- app/scripts.js
+++ app/scripts.js
@@ -12,6 +12,7 @@ const red = document.querySelector('.red');
 const green = document.querySelector('.green');
 const yellow = document.querySelector('.yellow');
 
+//criando ordem aleatória de cores
 let shuffleOrder = () => {
     let colorOrder = Math.floor(Math.random() * 4);
     order[order.length] = colorOrder;
@@ -23,6 +24,7 @@ let shuffleOrder = () => {
     }
 }
 
+//Acende a próxima cor
 let lightColor = (element, number) => {
     number = number * 500;
     setTimeout(() => {
@@ -33,3 +35,31 @@ let lightColor = (element, number) => {
         element.classList.remove('selected')
     })
 }
+
+
+//checa se os botões clicados são os mesmos da ordem gerada no jogo
+let checkOrder = () => {
+    for(let i in clickedOrder) {
+        if(clickedOrder[i] != order[i]) {
+            lose();
+            break;
+        }
+    }
+    if(clickedOrder.length == order.length){
+        alert(`Pontuação: ${score}\n Voce acertou! Iniciando próximo nível!`);
+        nextLevel();
+    }
+}
+
+//função para o clique do usuário
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    createColorElement(color).classList.add('selected');
+
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected');
+    })
+
+    checkOrder();
+}
+
